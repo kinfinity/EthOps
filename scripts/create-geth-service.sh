34fa33ca6a9ad414b684010076d44f96a33bf668
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2023-09-02 09:49:53 -0600

    setup vm & bootstrap geth service

--- scripts/create-geth-service.sh
+++ scripts/create-geth-service.sh
@@ -27,7 +27,6 @@ on_startup() {
 trap clean_up EXIT
 trap on_startup EXIT
 
-
 setup_service() {
     #  create the service file and copy it to system services folder
     EXEC_LINE="/bin/bash /usr/bin/${SERVICE_NAME}"
@@ -35,35 +34,37 @@ setup_service() {
     USER_GROUP="geth"
     DOCS_LINK="https://geth.ethereum.org/docs"
 
-    # Create a sed script file for multiline replacements
-    cat << EOF > sed_script.sed
-s/SERVICE_NAME/${SERVICE_NAME}/g
-s/EXEC_LINE/${EXEC_LINE}/g
-s/USER_NAME/${USER_NAME}/g
-s/USER_GROUP/${USER_GROUP}/g
-s/DOCS_LINK/${DOCS_LINK}/g
-EOF
+    ls -la
+    # Apply the sed replacement to the template file
+    sed -i "
+        s#SERVICE_NAME#${SERVICE_NAME}#g
+        s#EXEC_LINE#${EXEC_LINE}#g
+        s#USER_NAME#${USER_NAME}#g
+        s#USER_GROUP#${USER_GROUP}#g
+        s#DOCS_LINK#${DOCS_LINK}#g
+    " configs/void.service.template
+    sudo cp configs/void.service.template  "${SERVICE_NAME}.service"
 
-    # Apply the sed script to the template file
-    sed -f sed_script.sed ./configs/void.service.template > ./${SERVICE_NAME}.service
-    sudo cp ./${SERVICE_NAME}.service /etc/systemd/system/${SERVICE_NAME}.service
+    cat ${SERVICE_NAME}.service
+    sudo cp ${SERVICE_NAME}.service /etc/systemd/system/${SERVICE_NAME}.service
     sudo chmod 644 /etc/systemd/system/${SERVICE_NAME}.service
+    echo "DONE"
     
 }
 
 verify_service() {
+    # bug fix
+    sudo apt install --reinstall systemd -y --force-yes
     #  verify service
     sudo systemctl start $SERVICE_NAME
     sudo systemctl status $SERVICE_NAME
     sudo journalctl -u $SERVICE_NAME -r -o json-pretty
-    
 }
 
 # setup script
 BASE_NAME=$(basename -- ${SCRIPT_PATH})
 sudo cp ${SCRIPT_PATH} "/usr/bin/$(basename -- ${SCRIPT_PATH})"
 sudo chmod +x "/usr/bin/$(basename -- ${SCRIPT_PATH})"
-
 
 echo "Setup ${SERVICE_NAME} service..."
 setup_service
